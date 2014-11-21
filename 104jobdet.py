bdb4863de43316f6bbda9370ad020f17b788d7d5
jKodrum <jkodrum@gmail.com>
2014-11-21 10:02:01 +0800

    get spesified string but info of interview

--- 104jobdet.py
+++ 104jobdet.py
@@ -38,18 +38,30 @@ data = parse104(htmlfile)
 #f = open("replace.txt", "w")
 #f.write(data)
 #print data
-cnt=1
-print data[1:]
-'''
+essence=""
+#cnt=1
 for i in data:
-	print cnt
-	print re.sub(r"^\s+$","",i)
+	#print cnt
 	for j in re.split("\n",i):
+		# delete blank lines
 		if len(j)>0:
-			print j
-	cnt+=1
+			essence += (j+'\n')
+	#cnt+=1
 '''
 '''
-for i in data[1]:
-	print str(ord(i))+ ' '
-'''
+#print essence
+print "更新:" + re.search("(?<=更新日期：).*", essence).group(0)
+print "工作:" + re.search("(?<=工作內容\n).*", essence).group(0)
+print "待遇:" + re.search("(?<=工作待遇：\n).*", essence).group(0)
+print "地點:" + re.search("(?<=上班地點：\n).*", essence).group(0)
+print "時段:" + re.search("(?<=上班時段：\n).*", essence).group(0)
+print "休假:" + re.search("(?<=休假制度：\n).*", essence).group(0)
+print "上班:" + re.search("(?<=可上班日：\n).*", essence).group(0)
+print "人數:" + re.search("(?<=需求人數：\n).*", essence).group(0)
+print "身份:" + re.search("(?<=接受身份：\n).*", essence).group(0)
+print "學歷:" + re.search("(?<=學歷要求：\n).*", essence).group(0)
+print "學科:" + re.search("(?<=科系要求：\n).*", essence).group(0)
+print "語文:" + re.search("(?<=語文條件：\n).*", essence).group(0)
+print "擅長:" + re.search("(?<=擅長工具：\n).*", essence).group(0)
+print "技能:" + re.search("(?<=工作技能：\n).*", essence).group(0)
+print "其他:" + re.search("(?<=其他條件：\n).*", essence).group(0)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 104crawler
 ==========
+
+# readURL.py
+This program downloads a webpage from a given URL and writes to a given file name.
+[Usage]: `$ python readURL.py <URL> <outFile>`
